6c08995e39f876fd83815c445805275c2cb1bf25
Angel Pena <theangel3535@gmail.com>
2024-09-23 14:56:29 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+const infoArray = [
+  {
+    AcademicDep: 'ABC BS (Interdepartmental Concentration in Anthropology, Biology, & Chemistry)',
+    AppTypes: 'Zoom, Email, Phone',
+    Email: 'victoria.dominguez@lehman.cuny.edu',
+    link: 'http://www.lehman.edu/academics/anthropology',
+    notes: '',
+    group: 'UndAdv'
+  },
+  {
+    AcademicDep: 'Africana Studies',
+    AppTypes: 'Email, Phone, Blackboard, Zoom',
+    Email: 'africana.studies@lehman.cuny.edu',
+    link: 'https://www.lehman.edu/academics/arts-humanities/africana-studies/advising/',
+    notes: '',
+    group: 'UndAdv'
+  },
+  {
+    AcademicDep: 'Anthropology BA, Anthropology Minor, and Human Rights & Peace Studies Minor',
+    AppTypes: 'Zoom, Email, Phone',
+    Email: 'anthony.dest@lehman.cuny.edu',
+    link: 'http://www.lehman.edu/academics/anthropology',
+    notes: '',
+    group: 'UndAdv'
+  },
+  {
+    AcademicDep: 'Art',
+    AppTypes: 'Email, Virtual Phone',
+    Email: '',
+    link: 'https://www.lehman.edu/academics/arts-humanities/art/advising/',
+    notes: 'Follow link for instructions',
+    group: 'UndAdv'
+  },
+
+];
+
+
+$(document).ready(main());
+
+function main() {
+
+
+
+
+
+
+
+
+
+
+}
